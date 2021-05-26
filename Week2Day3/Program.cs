1fa8ae22c0cca37aae8b0d80df2c611a98c80f6e
Jacob Miller <JDMiller1995@gmail.com>
2021-05-26 19:16:02 -0400

    Add files via upload

---
+++ Week2Day3/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Week2Day3
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's get some info about what vehicle you're getting, and perform some calculations about it!"); //basic info gathering from user
+            Console.WriteLine("What make are you going to get?");
+            string make = Console.ReadLine();
+            carsTrucksVans vehicle1 = new carsTrucksVans(); //starting a new obj under class carsTrucksVans
+            Console.WriteLine("What model is it?");
+            vehicle1._model = Console.ReadLine();
+            Console.WriteLine("Awesome, so you're going to get a {0} {1}! What color will it be?", make, vehicle1._model);
+            vehicle1._color = Console.ReadLine();
+            Console.WriteLine("Is it electric or gas or diesal?");
+            vehicle1._fuel = Console.ReadLine();
+            Console.WriteLine("And how many MPG or miles per charge?");
+            int check() //this just makes sure they put a number in for MPG 
+            {
+                try
+                {
+                    vehicle1._MPG = Convert.ToInt32(Console.ReadLine());
+                    return vehicle1._MPG;
+                }
+                catch
+                {
+                    Console.WriteLine("Please put a number.");
+                            return check();
+                }
+            }
+            check(); //caling the function
+            Console.WriteLine("How many passengers can it hold?");
+            vehicle1._passengers = Console.ReadLine();
+            Console.WriteLine("Very cool, so far we have a {0} {1} {2} that runs on {3} and gets {4} miles per gallon or per charge, and can hold {5} passengers!", vehicle1._color, make, vehicle1._model, vehicle1._fuel, vehicle1._MPG, vehicle1._passengers);
+            Console.WriteLine("Alright these next few questions are so we can do some calculations!");
+            Console.Write("Initial cost is: "); //the below is setting values/making sure they are getting correct inputs 
+            try
+            {
+                vehicle1._intlCost = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("You wrote something that wasn't a number!");
+            }
+            Console.Write("Average yearly maintenance cost is: ");
+            try
+            {
+                vehicle1._avgMtcCost = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("You wrote something that wasn't a number!");
+            }
+            Console.WriteLine("How many miles will you be driving a year?");
+            try
+            {
+                vehicle1._milesPerYear = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("You wrote something that wasn't a number!");
+            }
+            vehicle1.timeToTravel(); //calls a function in my cars class
+            Console.WriteLine("Let's see how much your vehicle will cost per year on average!");
+            Console.WriteLine(vehicle1.cost(vehicle1._intlCost, vehicle1._avgMtcCost, vehicle1._MPG, vehicle1._milesPerYear)+ " per year"); //this calls a function in my cars class
+            void writeToFile() //writes everything to a file called CarInfo.txt, does overwrite anything else prior in the file though. 
+            {
+                StreamWriter writer1 = new StreamWriter("CarInfo.txt");
+                writer1.WriteLine("Your car is a {0} {1} {2} that get's {3} MPG/MPC and cost ${4}.", vehicle1._color, make, vehicle1._model, vehicle1._MPG, vehicle1._intlCost);
+                writer1.Close();
+            }
+            writeToFile();
+            try  //below reads the file i created above 
+            { 
+                
+                StreamReader reader1 = new StreamReader("CarInfo.txt");
+                string contents = reader1.ReadToEnd();
+                reader1.Close();
+                Console.WriteLine(contents);
+            }
+            catch (Exception e) //this is just an error message if for some reason the file is inaccesable 
+            {
+                
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+        
+        
+    }
+}
---
+++ Week2Day3/Transportation.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Day3
+{
+    abstract class Transportation //abstract class for transportation methods, could be road, water, air, etc. Should have all these values and the functions could work
+    {
+        protected string color;
+        protected int avgMtcCost; //avg maintanence cost over a year
+        protected int avgSpeed; //avg speed it can/will travel
+        protected int intlCost; //initial investment cost
+        public int _intlCost // the get set below are all set up so that we can assign these values
+        {
+            get
+            {
+                return intlCost;
+            }
+            set
+            {
+                intlCost = value;
+            }
+        }
+        public int _avgSpeed
+        {
+            get
+            {
+                return avgSpeed;
+            }
+            set
+            {
+                avgSpeed = value;
+            }
+        }
+        public string _color
+        {
+            get
+            {
+                return color;
+            }
+            set
+            {
+                color = value;
+            }
+        }
+        public int _avgMtcCost
+        {
+            get
+            {
+                return avgMtcCost;
+            }
+            set
+            {
+                avgMtcCost = value;
+            }
+        }
+        public abstract void timeToTravel(); //we'll use this to perform basic calcs like how long will this take to get somewhere
+
+        public virtual string cost() //this will be an avg yearly cost 
+        {
+            int i = 0;
+            string message = "Your car's average yearly cost is: ";
+            return message ;
+        }
+    }
+    abstract class Road : Transportation //abstract class for road transportation methods, any method of road transportation should have these attributes
+    {
+        protected string fuel; //diesal, electric, gas, human power, etc
+        protected int wheels;
+        protected int milesPerYear; //miles driven per year
+
+        public int _milesPerYear
+        {
+            get
+            {
+                return milesPerYear;
+            }
+            set
+            {
+                milesPerYear = value;
+            }
+        }
+        public int _wheels
+        {
+            get
+            {
+                return wheels;
+            }
+            set
+            {
+                wheels = value;
+            }
+        }
+        public string _fuel
+        {
+            get
+            {
+                return fuel;
+            }
+            set
+            {
+                fuel = value;
+            }
+        }
+        public override void timeToTravel() //so we can also carry these above functions
+        {
+            int i = 0;
+        }
+        public override string cost()
+        {
+            string message = "Your car will cost $";
+            return message;
+        }
+    }
+    class carsTrucksVans : Road //this class isn't abstract, it's for the named vehicles
+    {
+        private int MPGorMPC; //Miles per gallon or miles per charge if electric 
+        private string passengers; //how many passengers it can hold
+        private string model;
+        public string _model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public int _MPG
+        {
+            get
+            {
+                return MPGorMPC;
+            }
+            set
+            {
+                MPGorMPC = value;
+            }
+        }
+        public string _passengers
+        {
+            get
+            {
+                return passengers;
+            }
+            set
+            {
+                passengers = value;
+            }
+        }
+
+        public override void timeToTravel() //this asks how many miles someone is going to go, then returns the number to whatever called it. 
+        {
+            base.timeToTravel();
+            Console.WriteLine("Let's see how long it would take you to get somewhere!");
+            Console.WriteLine("Please input how many miles you'll be traveling in whole numbers!");
+            int miles;
+            int speed;
+            try
+            {
+                miles = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What will be your avg speed this trip in miles per hour?");
+                speed = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("This trip would take you {0} hours to complete!", miles / speed);
+
+            }
+            catch
+            {
+                Console.WriteLine("You have to enter whole numbers!");
+                 timeToTravel();
+            }
+            
+            
+           
+           
+        }
+        public string cost(int initial, int MTC, int MPG, int MPY) //these inputs are things we've already gathered from the user, and outputs the yearly cost 
+        {
+            Console.WriteLine("How many years do you expect to own your vehicle?");
+            try
+            {
+                int years = Convert.ToInt32(Console.ReadLine());
+                int yearlyCost = (initial / years) + MTC + (MPG / MPY);
+                return base.cost() + yearlyCost;
+            }
+            catch
+            {
+                Console.WriteLine("You have to input a number");
+                return cost();
+            }
+            
+            
+        }
+    }
+}
